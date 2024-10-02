d71c68909b3c701d4d763cc82c468aa52f95b3bb
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-02 18:17:50 -0500

    ES6 syntax functions

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function addNumbers(num1, num2) {
 }
 
 console.log(addNumbers(6, 54))
+
+const greet = function (name) {
+    return console.log("Hello " + name + " from a function expression");
+}
+
+greet("John");
+
+const multiply = (x, y, z) => x * y * z;
+
+console.log("Lambda function with ES6: ", multiply(2, 3, 4));
+
+const cubed = someNumber => someNumber * someNumber * someNumber;
+
+console.log("Arrow function with ES6: ", cubed(5));
