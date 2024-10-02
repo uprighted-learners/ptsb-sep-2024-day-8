828fc1e79d766b6aaa84cdacb9a01f40fbe70090
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-02 18:00:39 -0500

    basic functions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+let greeting = "Hello World from a variable";
+
+console.log(greeting);
+
+function functionName() {
+    console.log("Hello World from a function");
+}
+
+functionName();
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(addNumbers(6, 54))
