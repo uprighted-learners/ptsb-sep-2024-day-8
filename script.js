51eabd3156397c3ec265571f9b9578e76ac529de
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-02 19:00:26 -0500

    more functions

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ console.log("Lambda function with ES6: ", multiply(2, 3, 4));
 const cubed = someNumber => someNumber * someNumber * someNumber;
 
 console.log("Arrow function with ES6: ", cubed(5));
+
+let restaurant = "popeyes";
+console.log("name of a restaurant: ", restaurant.toUpperCase());
+
+let listOfNames = "john jerry harry sally sarah glenda";
+console.log("List of names: ", listOfNames);
+const listOfNamesArray = listOfNames.split(" ");
+console.log("List of names with `split`: ", listOfNamesArray);
+console.log("4th position should be sarah: ", listOfNamesArray[4].toUpperCase())
+
+let number = "123456789";
+console.log(typeof number);
+console.log("Number: ", parseInt(number));
+console.log(typeof parseInt(number));
+
+let price = 19.95;
+console.log("Price rounded up: ", price.toFixed(20));
